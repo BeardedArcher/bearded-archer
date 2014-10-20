1fa0c927517912c258788d4922cf3398e13a7411
Tuminure <tuminure@gmail.com>
2014-10-20 15:58:29 +0200

    debug

--- server/server.js
+++ server/server.js
@@ -1,29 +1,24 @@
 var fs = require('fs'), 
     http = require('http'),
     socketio = require('socket.io'),
-    user = require('./modules/users');
-
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end(fs.readFileSync(__dirname + '/index.html'));
-}).listen(8080, function() {
-    console.log('Listening at: http://localhost:8080');
-});
-
-var loggedUsers = [];
-
-user.register();
-
-socketio.listen(server).on('connection', function (socket) {
-    socket.on('login', function(data) {
-        console.log(data);
-        console.log('Tried to login: ', data.login, data.password);
+    user = require('./modules/users'),
+    debug = require('debug')('debug'),
+    server = http.createServer(function(req, res) {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end(fs.readFileSync(__dirname + '/index.html'));
+    }).listen(8080, function() {
+        debug('Listening at: http://localhost:8080');
     }),
-    socket.on('register', function(data) {
+    io = socketio.listen(server).on('connection', function (socket) {
+        var loggedUsers = {};
+        socket.on('login', function(data) {
+            debug(data);
+        }),
+        socket.on('register', function(data) {
 
-    }),
-    socket.on('createGame', function (data) {
-        console.log('Creating game: ', data.host, data.guest);
-        socket.broadcast.emit('message', msg);
+        }),
+        socket.on('createGame', function (data) {
+            console.log('Creating game: ', data.host, data.guest);
+            socket.broadcast.emit('message', msg);
+        });
     });
-});
