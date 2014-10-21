3fe319f4f43e74f85cc3ad77c204661e6b58b3a2
Tuminure <tuminure@gmail.com>
2014-10-21 16:02:38 +0200

    env

--- server/modules/dbconnection/index.js
+++ server/modules/dbconnection/index.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql'),
-    dbconfig = require('./database.json');
+    dbconfig = require(process.cwd() + '/database.json').dev,
+    connection = mysql.createConnection(dbconfig);
     
-exports.connection = mysql.createConnection(dbconfig);
+exports.connection = connection;
--- server/modules/users/index.js
+++ server/modules/users/index.js
@@ -1,5 +1,16 @@
-var mysql = require('mysql');
+var db = require(process.cwd() + '/modules/dbconnection').connection,
+    crypto = require('crypto');
 
-exports.register = function() {
-    mysql.query('SELECT * FROM users');
+exports.register = function(login, password) {
+    var salt = crypto.randomBytes(8).toString(),
+        data = {
+            login: login,
+            password: crypto.createHash('sha256').update(password + salt).digest('hex'),
+            salt: salt
+        };
+    db.query('INSERT INTO users SET ?', data);
+}
+
+exports.login = function() {
+    db.query('SELECT * FROM users');
 }
