f1c228c53e6befe3ea854ef77191841915f621a8
Tuminure <tuminure@gmail.com>
2014-10-30 15:09:04 +0100

    init

--- server/modules/dbconnection/index.js
+++
@@ -1,5 +0,0 @@
-var mysql = require('mysql'),
-    dbconfig = require(process.cwd() + '/database.json').dev,
-    connection = mysql.createConnection(dbconfig);
-    
-exports.connection = connection;
